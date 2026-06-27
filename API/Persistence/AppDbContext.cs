00a4a363a8eb78ea21f7cd97c49a5afe26a9b775
Eric Musyoka <musyokaeric@live.com>
2026-06-27 11:43:39 +0300

    Initial EFCore Migration

---
+++ API/Persistence/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using API.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Persistence;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Activity> Activities { get; set; }
+}
